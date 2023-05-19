7eee88db09ce0149294c6ab7fc581c173472a863
Liu Zhenshun <zhenshunliu1998@gmail.com>
2023-05-19 16:19:05 +0800

    change app name from consise to concise

--- label_studio_concise/asgi.py
+++ label_studio_concise/asgi.py
@@ -11,9 +11,9 @@ import os
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter,URLRouter
-from label_studio_consise import routing
+from label_studio_concise import routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'label_studio_consise.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'label_studio_concise.settings')
 
 #application = get_asgi_application()
 application = ProtocolTypeRouter({
